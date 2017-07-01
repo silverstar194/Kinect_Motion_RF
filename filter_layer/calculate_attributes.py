6a766d7aa68d4dde28e2b46427c2efd0c8711972
Sean <Admin@Seans-MacBook-Pro.local>
2017-06-30 23:15:08 -0700

    added sample files for filter layer

---
+++ filter_layer/calculate_attributes.py
@@ -0,0 +1,12 @@
+## This will be very simple to do
+class calualte_attributes:
+
+		class frame_calulate_attributes:
+			##ref to frame_island_classification
+			##new array/vector of computer attributes
+
+		def calcuate_attributes(array_of_pixel_offset):
+			##I will later define the pixel offsets as a constant
+			##likely will take a radian pattern
+			##reference https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/BodyPartRecognition.pdf
+
--- filter_layer/island_classification.py
+++ filter_layer/island_classification.py
@@ -1,7 +1,9 @@
 
 class island_classification:
 
-	class frame_island_forming:
+	##store for past frames that are used to compare across
+
+	class frame_island_classification:
 		##ref to island forming class
 		#array of human island(s)
 			# [	island1:[
@@ -16,4 +18,19 @@ class island_classification:
 			# 			(x,y)
 			# 			]
 			# ]
+
+	def get_human_islands():
+		##returns the frame_island_forming class
+		##with human islands array
+
+
+	def find_human islands():
+		##keep pulling frames from island_forming queue
+		##until a set is island is moving significantly
+
+
+	def move_significantly):
+		##a statistically significantly movement above the average movement
+		##can use a proportions test here
+
 
